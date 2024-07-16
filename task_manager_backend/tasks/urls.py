e987e1cbb8d8d7749f1d944b8dabd17f743e4c97
kessie <kayp3477@gmail.com>
2024-07-16 23:12:48 +0000

    Add URL routes for user list, detail, and task list views

--- task_manager_backend/tasks/urls.py
+++ task_manager_backend/tasks/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import UserListView, UserDetailView, TaskListView
 from .import views
 
 urlpatterns = [
     path('signup/', views.signup),
     path('login/', views.login),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('tasks/', TaskListView.as_view(), name='task-list'),
 ]
