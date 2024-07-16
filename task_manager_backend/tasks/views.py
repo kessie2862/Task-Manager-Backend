3e0db41a4120c1a319fcc9dc4eb9021487c1c32f
kessie <kayp3477@gmail.com>
2024-07-16 23:24:32 +0000

    Define signup view function to handle user creation with validation

--- task_manager_backend/tasks/views.py
+++ task_manager_backend/tasks/views.py
@@ -4,3 +4,20 @@ from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_simplejwt.tokens import RefreshToken
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            username = data.get('username')
+            password = data.get('password')
+            email = data.get('email')
+
+            if not username or not password:
+                return JsonResponse({'error': 'Username and password are required.'}, status=400)
+
+            user = User.objects.create_user(
+                username=username, password=password, email=email)
+            user.save()
