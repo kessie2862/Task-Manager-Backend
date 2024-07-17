738210a0225c41a868a1df888a9aa5c34dfd0279
kessie <kayp3477@gmail.com>
2024-07-17 00:09:48 +0000

    Define TaskSerializer class for serializing Task model fields

--- task_manager_backend/tasks/serializers.py
+++ task_manager_backend/tasks/serializers.py
@@ -7,3 +7,15 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    assigned_user = UserSerializer(read_only=True)
+    assigned_user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), write_only=True, source='assigned_user')
+
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'due_date',
+                  'status', 'assigned_user', 'assigned_user_id', 'created_by']
+        read_only_fields = ['created_by']
