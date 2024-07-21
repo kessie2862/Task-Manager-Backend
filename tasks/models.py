58368b4aeb4b969807b41b89bf62c5be3b61f93b
kessie <kayp3477@gmail.com>
2024-07-21 19:36:47 +0000

    Add priority column to model

--- tasks/models.py
+++ tasks/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     due_date = models.DateField()
@@ -13,6 +19,8 @@ class Task(models.Model):
         ('in_progress', 'In Progress'),
         ('completed', 'Completed')
     ])
+    priority = models.CharField(
+        max_length=10, choices=PRIORITY_CHOICES, default='medium')
     assigned_user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='tasks')
     created_by = models.ForeignKey(
